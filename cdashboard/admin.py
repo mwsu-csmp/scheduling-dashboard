0b604e9619eaf02ee07a39431736c2511b79d04c
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-02-02 14:01:24 -0600

    removed database (not needed)

--- cdashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-""" This file will not be needed. It was decided that admin rights will be determined elsewhere """
-""" But for right now Django needs an admin.py  """
-
-class CurriculumAdmin(admin.ModelAdmin):
-    """ This is not needed """
-    fields = ['pub_date', 'question_text']
